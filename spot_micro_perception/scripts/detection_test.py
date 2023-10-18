6bd77f64140c5f986425bf1c85b3a1fb562da617
spotty <spotty@SpotMicro.myguest.virtualbox.org>
2023-10-18 13:15:12 +0200

    refactor to improve loading from json

--- spot_micro_perception/scripts/detection_test.py
+++ spot_micro_perception/scripts/detection_test.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python
 
-# from os.path import dirname, abspath
 import cv2
 import numpy as np
 import rospkg
 import json
+import os
+
 
 rospack = rospkg.RosPack()
 pkgdir = rospack.get_path('spot_micro_perception')
 
-
-model = cv2.dnn.readNetFromDarknet(pkgdir + '/configs/networks/yolov2.cfg', pkgdir + '/configs/networks/yolov2.weights')
-
-
-classes = []
-with open(pkgdir + '/configs/labels/coco.txt') as voc:
-   classes = voc.read().splitlines()
-
-print(classes)
-
-
-data = {}
-with open(pkgdir + '/configs/camera_recording_size.json', 'r') as f:
+with open(pkgdir + '/configs/models_dictionary.json', 'r') as f:
    data = json.load(f)
 
-print(data)
-print(data['yolov2'])
+model_name = 'yolov2'
+model_setup = data[model_name]
 
+
+width, height,  = model_setup['width'], model_setup['height']
+
+cfg = model_setup['cfg']
+cfg_path = os.path.join(pkgdir, cfg)
+
+weight = model_setup['weight']
+weight_path = os.path.join(pkgdir, weight)
+
+model = cv2.dnn.readNetFromDarknet(cfg_path, weight_path)
+
+
+labels = model_setup['labels']
+labels_path = os.path.join(pkgdir, labels)
+
+with open(labels_path) as voc:
+   classes = voc.read().splitlines()
 
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
@@ -34,8 +40,7 @@ colors = np.random.uniform(0, 255, size=(len(classes), 3))
 capture = cv2.VideoCapture(0)
 while capture.isOpened():
    success, image = capture.read()
-   image_height, image_width, channels = image.shape
-   print(success, image_height, image_width)
+   # image_height, image_width, channels = image.shape
    break
 
 
