94a1d74e4fb0ae0b997768c3dbcf568a199f299f
spotty <spotty@SpotMicro.myguest.virtualbox.org>
2023-10-18 11:28:21 +0200

    refactoring folders and implementing json configs

--- spot_micro_perception/scripts/detection_test.py
+++ spot_micro_perception/scripts/detection_test.py
@@ -4,13 +4,40 @@
 import cv2
 import numpy as np
 import rospkg
+import json
 
 rospack = rospkg.RosPack()
 pkgdir = rospack.get_path('spot_micro_perception')
 
-model = cv2.dnn.readNetFromDarknet(pkgdir + '/network_config/yolo-voc.cfg', pkgdir + '/network_config/yolo-voc.weights')
+
+model = cv2.dnn.readNetFromDarknet(pkgdir + '/configs/networks/yolov2.cfg', pkgdir + '/configs/networks/yolov2.weights')
+
 
 classes = []
-
-with open(pkgdir + '/object_labels/voc.txt') as voc:
+with open(pkgdir + '/configs/labels/coco.txt') as voc:
    classes = voc.read().splitlines()
+
+print(classes)
+
+
+data = {}
+with open(pkgdir + '/configs/camera_recording_size.json', 'r') as f:
+   data = json.load(f)
+
+print(data)
+print(data['yolov2'])
+
+
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+
+capture = cv2.VideoCapture(0)
+while capture.isOpened():
+   success, image = capture.read()
+   image_height, image_width, channels = image.shape
+   print(success, image_height, image_width)
+   break
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
