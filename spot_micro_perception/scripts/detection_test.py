b3f9bec93614f3fb89749519319c14046734442d
spotty <spotty@SpotMicro.myguest.virtualbox.org>
2023-10-18 14:22:25 +0200

    some initial testing of detections

--- spot_micro_perception/scripts/detection_test.py
+++ spot_micro_perception/scripts/detection_test.py
@@ -17,7 +17,7 @@ model_name = 'yolov2'
 model_setup = data[model_name]
 
 
-width, height,  = model_setup['width'], model_setup['height']
+model_width, model_height,  = model_setup['width'], model_setup['height']
 
 cfg = model_setup['cfg']
 cfg_path = os.path.join(pkgdir, cfg)
@@ -25,7 +25,7 @@ cfg_path = os.path.join(pkgdir, cfg)
 weight = model_setup['weight']
 weight_path = os.path.join(pkgdir, weight)
 
-model = cv2.dnn.readNetFromDarknet(cfg_path, weight_path)
+network = cv2.dnn.readNetFromDarknet(cfg_path, weight_path)
 
 
 labels = model_setup['labels']
@@ -40,7 +40,11 @@ colors = np.random.uniform(0, 255, size=(len(classes), 3))
 capture = cv2.VideoCapture(0)
 while capture.isOpened():
    success, image = capture.read()
-   # image_height, image_width, channels = image.shape
+   image_height, image_width, channels = image.shape
+   blob = cv2.dnn.blobFromImage(cv2.resize(image, (model_height, model_width)))
+   network.setInput(blob)
+   detections = network.forward()
+   print(detections)
    break
 
 
