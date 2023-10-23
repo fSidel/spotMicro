4174e7c48574bf8a3c0945a75056f10464818d7f
spotty <spotty@SpotMicro.myguest.virtualbox.org>
2023-10-23 11:57:53 +0200

    initial refactoring in OOP

---
+++ spot_micro_perception/scripts/spotMicroObjectDetection.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+Class for sending coordinates of detected objects to spot micro walk and angle node
+"""
+import rospy  
+import sys
+import os
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import numpy as np
+import cv2
+
+
+class SpotMicroObjectDetection():  
+    nodeName = "object_detection_publisher"
+    topicName = "detection_topic" 
+    subcribedTopic = "video_topic"
+
+
+    def __init__(self):
+        rospy.init_node(self.nodeName, 
+                        anonymous=True)
+
+
+
+    def callbackFunction(self):
+        pass
+
+
+    def run(self):
+        rospy.Subscriber(self.subscribedTopic, 
+                         Image, 
+                         self.callbackFunction)
+
+
+if __name__ == "__main__":
+    smod = SpotMicroObjectDetection()
+    smod.run()
