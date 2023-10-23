6a0b3427aee10c6b42c44f7fd2485043a60cdfee
spotty <spotty@SpotMicro.myguest.virtualbox.org>
2023-10-23 12:46:10 +0200

    i was wrong lol

--- spot_micro_perception/scripts/camera_publisher.py
+++ spot_micro_perception/scripts/camera_publisher.py
@@ -5,35 +5,39 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 
-"""
-This Node will be used to publish the images as an image data type in
-our ROS Topic. This data needs to be converted into a OpenCV Image (type cv::Mat)
-because it is the type expected by our image recognition algorithms. To archieve
-this we will use CvBridge to conveniently convert between the two types.
-"""
 
-publisherNodeName='camera_sensor_publisher'
-topicName='video_topic'
-
-rospy.init_node(publisherNodeName,anonymous=True)
-publisher=rospy.Publisher(topicName,Image, queue_size=60)
-rate = rospy.Rate(30)
-
-videoCaptureObject=cv2.VideoCapture(0)
-bridgeObject=CvBridge()
+class CameraPublisher():
+    nodeName = "camera_sensor_publisher"
+    topicName = "video_topic"
 
 
-while not rospy.is_shutdown():
-	"""
-	frameStatus: boolean
-	capturedFrame: cv image
-	imageToTransmit: ros image
-	"""
+    def __init__(self):
+        rospy.init_node(self.nodeName,
+                        anonymous=True)
+        
+        self.capture_pub = rospy.Publisher(self.topicName, Image, queue_size=60)
+        self.rate = rospy.Rate(30)
 
-	frameStatus, capturedFrame = videoCaptureObject.read()
-	
-	if frameStatus == True:
-		rospy.loginfo('Video frame captured and published')
-		imageToTransmit=bridgeObject.cv2_to_imgmsg(capturedFrame)
-		publisher.publish(imageToTransmit)
-	rate.sleep()
+        self.videoCaptureObject = cv2.VideoCapture(0)
+        self.bridgeObject = CvBridge()
+        
+    
+    def videoCapture(self):
+        frameStatus, capturedFrame = self.videoCaptureObject.read()
+
+        if frameStatus == True:
+            rospy.loginfo("Video frame captured and published")
+            imageToPublish = self.bridgeObject.cv2_to_imgmsg(capturedFrame)
+            self.capture_pub.publish(imageToPublish)
+
+        self.rate.sleep()
+
+    
+    def run(self):
+        while not rospy.is_shutdown():
+            self.videoCapture()
+
+
+if __name__ == "__main__":
+    cp = CameraPublisher()
+    cp.run()
