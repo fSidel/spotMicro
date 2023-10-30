abbb9fb09f282b207ecb07b806326d34e7e4880c
spotty <spotty@SpotMicro.myguest.virtualbox.org>
2023-10-30 14:23:34 +0100

    bit of a rewrite

--- spot_micro_perception/scripts/spotMicroPerceptionMove.py
+++ spot_micro_perception/scripts/spotMicroPerceptionMove.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import Float32, Bool
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import Twist
-from spot_micro_perception.msg import Detection
+from spot_micro_perception.msg import Detections
 from math import pi
 import cv2
 
@@ -12,7 +12,13 @@ import cv2
 class SpotMicroPerceptionControl():
     node_name = "perception_control_node"
     detection_subscription = "detection_topic"
+    angle_publication = "/angle_cmd"
+    velocity_publication = "/cmd_vel"
+    walk_publication = "/walk_cmd"
+    stand_publication = "/stand_cmd"
+    idle_publication = "/idle_cmd"
 
+    
     def __init__(self):
 
         self._angle_cmd_msg = Vector3()
@@ -29,7 +35,7 @@ class SpotMicroPerceptionControl():
         self._vel_cmd_msg.angular.z = 0
 
         self._walk_event_cmd_msg = Bool()
-        self._walk_event_cmd_msg.data = True  # Mostly acts as an event driven action on receipt of a true message
+        self._walk_event_cmd_msg.data = True 
 
         self._stand_event_cmd_msg = Bool()
         self._stand_event_cmd_msg.data = True
@@ -43,22 +49,22 @@ class SpotMicroPerceptionControl():
                         anonymous=True)
 
         # Create publishers for commanding velocity, angle, and robot states
-        self._ros_pub_angle_cmd = rospy.Publisher('/angle_cmd', 
+        self._ros_pub_angle_cmd = rospy.Publisher(self.angle_publication, 
                                                   Vector3, 
                                                   queue_size=1)
         
-        self._ros_pub_vel_cmd = rospy.Publisher('/cmd_vel', 
+        self._ros_pub_vel_cmd = rospy.Publisher(self.velocity_publication, 
                                                 Twist, 
                                                 queue_size=1)
         
-        self._ros_pub_walk_cmd = rospy.Publisher('/walk_cmd', 
+        self._ros_pub_walk_cmd = rospy.Publisher(self.walk_publication, 
                                                  Bool, 
                                                  queue_size=1)
-        self._ros_pub_stand_cmd = rospy.Publisher('/stand_cmd', 
+        self._ros_pub_stand_cmd = rospy.Publisher(self.stand_publication, 
                                                   Bool, 
                                                   queue_size=1)
         
-        self._ros_pub_idle_cmd = rospy.Publisher('/idle_cmd', 
+        self._ros_pub_idle_cmd = rospy.Publisher(self.idle_publication, 
                                                  Bool, 
                                                  queue_size=1)
 
@@ -77,6 +83,7 @@ class SpotMicroPerceptionControl():
 
         self._ros_pub_vel_cmd.publish(self._vel_cmd_msg)
 
+
     def reset_all_angle_commands_to_zero(self):
         '''Reset angle cmd states to zero and publish them'''
 
@@ -87,15 +94,16 @@ class SpotMicroPerceptionControl():
         self._ros_pub_angle_cmd.publish(self._angle_cmd_msg)
 
     
-    def on_detection(self):
+    def detection_callback(self, message):
+
         pass
 
     def run(self):
         self.reset_all_motion_commands_to_zero()
         
         rospy.Subscriber(self.detection_subscription, 
-                         Detection, 
-                         self.on_detection)
+                         Detections, 
+                         self.detection_callback)
 
         rospy.spin()
 
